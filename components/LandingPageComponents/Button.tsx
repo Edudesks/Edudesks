a374dcc78bafc73014476580336c22f42d44d716
Prom1234pro <promiseimadonmwinyi@gmail.com>
2024-11-04 12:44:54 +0100

    completed the landing page

--- components/LandingPageComponents/Button.tsx
+++ components/LandingPageComponents/Button.tsx
@@ -13,9 +13,9 @@ const ButtonTrial: FC<ButtonTrialProps> = ({ variant, text, url }) => {
   const arrowSrc = variant === 'light' ? "arrow-right-light.svg" : "arrow-right-dark.svg";
 
   return (
-    <div className={`${styles.button} ${variant === 'light' ? styles.light : styles.dark}`}>
+    <Link href={url} className={`${styles.button} ${variant === 'light' ? styles.light : styles.dark}`}>
       <button className={`${styles.btn} ${variant === 'light' ? styles.btnLight : styles.btnDark}`}>
-        <Link href={url}>{text}</Link>
+        {text}
       </button>
       <Image
         src={`/icons/${arrowSrc}`}
@@ -24,7 +24,7 @@ const ButtonTrial: FC<ButtonTrialProps> = ({ variant, text, url }) => {
         width={25}
         height={10}
       />
-    </div>
+    </Link>
   );
 };
 
