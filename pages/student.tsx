9730eb5e56e6419e3f53c1082034aaf963d8b08e
Laura Ogbechie <ogbechielaura04@gmail.com>
2024-11-13 14:30:25 +0100

    completed imageUploadComponent

---
+++ pages/student.tsx
@@ -0,0 +1,17 @@
+import FormStepComponent from '@/components/StudentComponent/FormStepComponent'
+import React from 'react'
+/**
+ *
+ * TODO: Add further functionalities to image component. User should be able to center image.
+ * TODO: On click, user should also be able to edit image.
+ */
+
+const Student = () => {
+  return (
+    <div>Student
+        <FormStepComponent step={0} />
+    </div>
+  )
+}
+
+export default Student
