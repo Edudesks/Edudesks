b7adf8268db3d396b8cd9619de235e6da05fd8db
Chizaram <chukwumaobidivine87@gmail.com>
2024-11-18 20:35:09 +0100

    changes

--- pages/student-profile.tsx
+++ pages/student-profile.tsx
@@ -123,7 +123,7 @@ export default function StudentProfile() {
         <div className="block xl:hidden">
         <StudentRegisterDetails/>
         </div>
-            <div className="flex md:flex-row flex-col items-center gap-5 xl:gap-0 xl:justify-between w-full">
+            <div className="flex lg:flex-row flex-col items-center gap-5 xl:gap-0 xl:justify-between w-full">
               {/* general profile */}
             <div className="bg-[#FFFFFF] w-full xl:w-[275px] h-auto sm:h-[324px] rounded-[15px] border border-[var(--border)] px-[15px] py-[16px] flex flex-col gap-[24px]">
               <h3 className="font-[600] text-[24px] leading-[38px] text-[var(--primary-text-color)]">General</h3>
@@ -187,9 +187,9 @@ export default function StudentProfile() {
             </div>
             </div>
             
-           <div className="flex items-center flex-col gap-y-6 justify-between w-full">
+           <div className="flex items-center flex-col lg:flex-row gap-x-6 gap-y-6 justify-between w-full">
              {/* Parent Information */}
-             <div className="bg-[#FFFFFF] w-full h-auto rounded-[15px] border border-[var(--border)] px-[15px] py-[16px] flex flex-col gap-[24px]">
+             <div className="bg-[#FFFFFF] w-full lg:w-[450px] h-auto lg:h-[400px] rounded-[15px] border border-[var(--border)] px-[15px] py-[16px] flex flex-col gap-[24px]">
               <h3 className="font-[600] text-[24px] leading-[38px] text-[var(--primary-text-color)]">Parents’ Information</h3>
               <section className="flex flex-col gap-[24px]">
               <div className="flex items-center gap-[16px]">
@@ -232,7 +232,7 @@ export default function StudentProfile() {
             </div>
 
             {/* Health */}
-            <div className="bg-[#FFFFFF] w-full h-[324px] rounded-[15px] border border-[var(--border)] px-[15px] py-[16px] flex flex-col gap-[24px]">
+            <div className="bg-[#FFFFFF] h-auto w-full lg:w-[300px] lg:h-[400px] rounded-[15px] border border-[var(--border)] px-[15px] py-[16px] flex flex-col gap-[24px]">
               <h3 className="font-[600] text-[24px] leading-[38px] text-[var(--primary-text-color)]">Health</h3>
               <section className="flex flex-col gap-[24px]">
               <div className="flex items-center gap-[16px]">
