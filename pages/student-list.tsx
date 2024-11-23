a3dd2a6fdc2e78ac3f04650a3392cb6864d1910c
Prom1234pro <promiseimadonmwinyi@gmail.com>
2024-11-23 16:15:05 +0100

    updated the dashboard

--- pages/student-list.tsx
+++
@@ -1,9 +0,0 @@
-import StudentFilterProfile from "@/components/StudentComponent/StudentFilterProfile";
-
-export default function StudentList (){
-    return(
-        <>
-            <StudentFilterProfile/>
-        </>
-    )
-}
