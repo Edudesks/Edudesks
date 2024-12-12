3b8d4cc91613f3ebca808100c3227a5d4e57483e
Chizaram <chukwumaobidivine87@gmail.com>
2024-12-12 19:47:58 +0100

    changes

--- pages/page-not-found.tsx
+++ pages/page-not-found.tsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import Link from "next/link";
 import { inter, openSans } from "@/app/fonts/fonts";
 import { useState, useEffect } from "react";
+import Navbar from "@/components/LandingPageComponents/NavBar";
+import Footer from "@/components/LandingPageComponents/Footer";
 export default function PageNotFound (){
     const [windowWidth, setWindowWidth] = useState<number>(0);
 
@@ -16,15 +18,16 @@ export default function PageNotFound (){
 
   const isBelow1328 = windowWidth <= 1328 && windowWidth >=1024;
     return(
-        <>
-            <div className={`${inter.className} flex lg:h-[509px] h-full items-start justify-center mt-[2rem]  px-10 `}>
+        <div className="bg-[var(--background)]">
+        <Navbar/>
+            <div className={`${inter.className} flex lg:h-[509px] h-full items-start justify-center mt-[2rem] px-10`}>
                 <div className="flex items-center justify-between h-full w-full lg:flex-row flex-col lg:gap-0 gap-10">
 
                 {/* media section */}
                 <section className={` ${isBelow1328?'w-[400px]':'w-[300px] sm:w-[565.94px]'} h-[350px]  flex justify-center  lg:hidden`}>
                 <div className='h-full'>
                   <Image
-                    src={"/icons/page_not_found_2.svg"}
+                    src={"/icons/page_not_found_1.svg"}
                     alt="employees icon"
                     width={1000}
                     height={1000}
@@ -35,7 +38,7 @@ export default function PageNotFound (){
                 </section>
 
                 {/* section 1 */}
-                <section className="flex w-full  flex-col text-[black] gap-[35px] items-center lg:pb-0 pb-8 lg:items-start h-[350px] justify-center">
+                <section className="flex w-full h-full  flex-col text-[black] gap-[35px] items-center lg:pb-0 pb-8 lg:items-start justify-center">
                     <h1 className={`text-[65px] sm:text-[128px] font-bold leading-[80px]`}>oooops!</h1>
                     <h3 className='text-[16px] sm:text-[40px] font-bold'>404 - PAGE IN PROGRESS </h3>
                     <p className="text-[14px] sm:text-[20px] text-center">The page you are looking for is temporarily unavailable </p>
@@ -43,8 +46,8 @@ export default function PageNotFound (){
                 </section>
 
                 {/* section 2 */}
-                <section className="h-[350px] w-full lg:block hidden">
-                <div className={`${isBelow1328?'w-[400px]':'w-[565.94px]'} h-full`}>
+                <section className="h-full w-full lg:block hidden">
+                <div className={`${isBelow1328?'w-[600px]':'w-[765.94px]'} h-full`}>
                   <Image
                     src={"/icons/page_not_found_2.svg"}
                     alt="employees icon"
@@ -56,6 +59,7 @@ export default function PageNotFound (){
                 </div>
                 </section></div>
             </div>
-        </>
+            <Footer/>
+        </div>
     )
 }
