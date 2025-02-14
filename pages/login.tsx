e62d0445b35453a0f08a313d4aac7a821f1203eb
Laura Ogbechie <ogbechielaura04@gmail.com>
2025-02-14 09:22:47 +0100

    removed border radius in signup and login pages

--- pages/login.tsx
+++ pages/login.tsx
@@ -126,12 +126,12 @@ const Login: React.FC = () => {
 
   return (
     <>
-    { isCheckingAuth ? "" : 
+    { isCheckingAuth ? "" :
       <div
         className={`${openSans.className} flex flex-col lg:flex-row gap-[3.375rem] lg:gap-[8.5625rem] h-screen whitespace-nowrap`}
       >
         {/* -------- left half of signup page -------- */}
-        <div className="flex flex-col align-top gap-[1.34125rem] lg:gap-[1.9375rem] bg-[var(--background)] h-full pt-[1.625rem] pl-[1.125rem] lg:px-[3.9375rem] lg:py-[1.875rem] rounded-t-none rounded-b-[30px] lg:rounded-e-[30px] lg:rounded-bl-none max-h-[24.5625rem] lg:max-h-full lg:w-[44.875rem]">
+        <div className="flex flex-col align-top gap-[1.34125rem] lg:gap-[1.9375rem] bg-[var(--background)] h-full pt-[1.625rem] pl-[1.125rem] lg:px-[3.9375rem] lg:py-[1.875rem] max-h-[24.5625rem] lg:max-h-full lg:w-[44.875rem]">
           <AuthentificationLogo />
           <div className="w-[16.625rem] lg:w-[33.6875rem] self-center">
             <Image
